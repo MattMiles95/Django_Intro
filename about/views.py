66b1b0ccf0e6e4b57a57ad5dd5fb1607239d5f38
Matt Miles <matt_miles2@live.co.uk>
2024-09-27 17:53:09 +0000

    Add function to 'about' app and connect to urls.py

--- about/views.py
+++ about/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def about_me(request):
+    return HttpResponse("Hello from the 'about' app, World!")
